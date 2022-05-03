6f010d0701539996070e1e97b2aea1ebb81d5ef3
rickydamarsaputra <retrocode.rc@gmail.com>
2022-05-03 12:00:29 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Website company profile for PT RIZKI JAYA ABADI
---
+++ resources/views/index.blade.php
@@ -0,0 +1,362 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>PT. Rizki Jaya Abadi Transportasi</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/slicknav.css">
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <!--? Preloader Start -->
+    <div id="preloader-active">
+        <div class="preloader d-flex align-items-center justify-content-center">
+            <div class="preloader-inner position-relative">
+                <div class="preloader-circle"></div>
+                <div class="preloader-img pere-text">
+                    <img src="assets/img/logo/loder.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Preloader Start -->
+    <header>
+        <!-- Header Start -->
+        <div class="header-area">
+            <div class="main-header">
+                <div class="header-bottom  header-sticky">
+                    <div class="container">
+                        <div class="row align-items-center">
+                            <!-- Logo -->
+                            <div class="col-xl-2 col-lg-2">
+                                <div class="logo">
+                                    <a href="index.html"><img src="assets/img/logo/logo.png" alt=""></a>
+                                </div>
+                            </div>
+                            <div class="col-xl-10 col-lg-10">
+                                <div class="menu-wrapper  d-flex align-items-center justify-content-end">
+                                    <!-- Main-menu -->
+                                    <div class="main-menu d-none d-lg-block">
+                                        <nav>
+                                            <ul id="navigation">
+                                                <li><a href="index.html">Home</a></li>
+                                                <li><a href="about.html">About</a></li>
+                                                <li><a href="services.html">Services</a></li>
+                                                <li><a href="contact.html">Contact</a></li>
+                                            </ul>
+                                        </nav>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Mobile Menu -->
+                            <div class="col-12">
+                                <div class="mobile_menu d-block d-lg-none"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Header End -->
+    </header>
+    <main>
+        <!--? slider Area Start-->
+        <div class="slider-area ">
+            <div class="slider-active">
+                <!-- Single Slider -->
+                <div class="single-slider slider-height d-flex align-items-center">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xl-10 col-lg-10">
+                                <div class="hero__caption">
+                                    <h1>Kami Melayani <span>Pengiriman</span> Barang - Barang Retail Ke Seluruh Indonesia</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="single-slider slider-height d-flex align-items-center">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xl-10 col-lg-10">
+                                <div class="hero__caption">
+                                    <h1>Kami Melayani <span>Pengiriman</span> Barang - Barang Retail Ke Seluruh Indonesia</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="single-slider slider-height d-flex align-items-center">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xl-10 col-lg-10">
+                                <div class="hero__caption">
+                                    <h1>Kami Melayani <span>Pengiriman</span> Barang - Barang Retail Ke Seluruh Indonesia</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- slider Area End-->
+        <!--? Categories Area Start -->
+        <div class="categories-area section-padding30">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <!-- Section Tittle -->
+                        <div class="section-tittle text-center mb-80">
+                            <span>Our Services</span>
+                            <h2>What We Can Do For You</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg">
+                        <div class="single-cat active text-center mb-20">
+                            <div class="cat-icon">
+                              <i class="fa-solid fa-map-location-dot"></i>
+                            </div>
+                            <div class="cat-cap">
+                                <h5>Tracking Barang</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg">
+                        <div class="single-cat text-center mb-20">
+                            <div class="cat-icon">
+                              <i class="fa-solid fa-truck"></i>
+                            </div>
+                            <div class="cat-cap">
+                                <h5>Trucking</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg">
+                        <div class="single-cat text-center mb-20">
+                            <div class="cat-icon">
+                              <i class="fa-solid fa-map-location-dot"></i>
+                            </div>
+                            <div class="cat-cap">
+                                <h5>Retail</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg">
+                        <div class="single-cat text-center mb-20">
+                            <div class="cat-icon">
+                              <i class="fa-solid fa-map-location-dot"></i>
+                            </div>
+                            <div class="cat-cap">
+                                <h5>Freight Forwarding</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg">
+                        <div class="single-cat text-center mb-20">
+                            <div class="cat-icon">
+                              <i class="fa-solid fa-box"></i>
+                            </div>
+                            <div class="cat-cap">
+                                <h5>Project Cargo</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg">
+                        <div class="single-cat text-center mb-20">
+                            <div class="cat-icon">
+                              <i class="fa-solid fa-truck-fast"></i>
+                            </div>
+                            <div class="cat-cap">
+                                <h5>Shipping Service</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <form action="">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Track your shipments</label>
+                                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                                    </div>
+                                    <button type="submit" class="btn">Track</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Categories Area End -->
+        <!--? About Area Start -->
+        <div class="about-low-area padding-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-12">
+                        <div class="about-caption mb-50">
+                            <!-- Section Tittle -->
+                            <div class="section-tittle mb-35">
+                                <span>About Our Company</span>
+                                <h2>Solusi Logistik & Transportasi Aman yang Menghemat Waktu Kita yang Berharga!</h2>
+                            </div>
+                            <p>
+                              PT. Rizki Jaya Abadi Transportasi Berdiri pada tanggal
+                              15 Juni 2009. Sebelumnya PT. Rizki Jaya Abadi
+                              berbadan hukum. Perseroan Komanditer dengan
+                              nama CV. Rizki Jaya Abadi dibawah wewenang notaris
+                              Ayem Agustinah., SH. Di Surabaya. Perseroan Terbatas
+                              ini didirikan untuk jangka waktu yang tidak terbatas,
+                              tertanggal sejak keputusan ini diturunkan oleh instansi
+                              pemerintah terkait yang mempunyai wewenang.
+                            </p>
+                            <p>
+                              Didalam naungan PT. Rizki Jaya Abadi Transportasi
+                              telah tergabung tenaga-tenaga profesional dari
+                              bidang terkait yang mempunyai etos kerja yang tinggi
+                              serta dedikasi tinggi terhadap pekerjaan dalam
+                              membangun perusahaan PT. Rizki Jaya Abadi
+                              Transportasi guna memberikan keseimbangan
+                              terhadap perkembangan zaman dan laju tumbuh
+                              perekonomian Indonesia dan Masyarakat Ekonomi
+                              Asean (MEA)
+                            </p>
+                            <a href="about.html" class="btn">More About Us</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12">
+                      <img src="assets/img/gallery/about2.png" alt="" class="w-100 float-right">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- About Area End -->
+    </main>
+    <footer>
+        <!--? Footer Start-->
+        <div class="footer-area footer-bg">
+            <div class="container">
+                <div class="footer-top footer-padding">
+                    <!-- Footer Menu -->
+                    <div class="row d-flex">
+                        <div class="col-xl-5 col-lg-5 col-md-5 col-sm-6">
+                            <div class="single-footer-caption mb-50">
+                                <h1 class="text-white">PT. Rizki Jaya Abadi Transportasi</h1>
+                                <div class="footer-tittle">
+                                    <div class="footer-pera">
+                                        <p class="info1">
+                                          Pengiriman Barang Menggunakan COLT DIESEL, FUSO ENGKEL, TRONTON, TRAILER, LOW BED, CONTAINER & KAPAL LOS CARGO? Melalui Darat dan Laut? KAMI SOLUSINYA !!! Kami melayani pengiriman barang partai besar maupun kecil ke seluruh wilayah Indonesia.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="footer-tittle">
+                                  <div class="footer-pera">
+                                      <p class="info1">
+                                        Email: rizkijayaabadi@ymail.com
+                                      </p>
+                                  </div>
+                                </div>
+                                <!-- Footer Social -->
+                                <div class="footer-social ">
+                                    <a href="https://www.facebook.com/sai4ull"><i class="fab fa-facebook-f"></i></a>
+                                    <a href=""><i class="fab fa-twitter"></i></a>
+                                    <a href="#"><i class="fas fa-globe"></i></a>
+                                    <a href="#"><i class="fab fa-instagram"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-lg-2 col-md-4 col-sm-6">
+                            <div class="single-footer-caption mb-50">
+                                <div class="footer-tittle">
+                                    <h4>COMPANY</h4>
+                                    <ul>
+                                        <li><a href="#">About Us</a></li>
+                                        <li><a href="#">Company</a></li>
+                                        <li><a href="#"> Press & Blog</a></li>
+                                        <li><a href="#"> Privacy Policy</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Footer Bottom -->
+                <div class="footer-bottom">
+                    <div class="row d-flex align-items-center">
+                        <div class="col-lg-12">
+                            <div class="footer-copy-right text-center">
+                                <p>
+                                    Copyright &copy; <script>document.write(new Date().getFullYear());</script> <span class="text-orange">PT. Rizki Jaya Abadi Transportasi</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer End-->
+    </footer>
+    <!-- Scroll Up -->
+    <div id="back-top">
+        <a title="Go to Top" href="#"> <i class="fas fa-level-up-alt"></i></a>
+    </div>
+
+    <!-- JS here -->
+
+    <script src="assets/js/vendor/modernizr-3.5.0.min.js"></script>
+    <!-- Jquery, Popper, Bootstrap -->
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- Jquery Mobile Menu -->
+    <script src="assets/js/jquery.slicknav.min.js"></script>
+
+    <!-- Jquery Slick , Owl-Carousel Plugins -->
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/slick.min.js"></script>
+    <!-- One Page, Animated-HeadLin -->
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/animated.headline.js"></script>
+    <script src="assets/js/jquery.magnific-popup.js"></script>
+
+    <!-- Nice-select, sticky -->
+    <script src="assets/js/jquery.nice-select.min.js"></script>
+    <script src="assets/js/jquery.sticky.js"></script>
+
+    <!-- contact js -->
+    <script src="assets/js/contact.js"></script>
+    <script src="assets/js/jquery.form.js"></script>
+    <script src="assets/js/jquery.validate.min.js"></script>
+    <script src="assets/js/mail-script.js"></script>
+    <script src="assets/js/jquery.ajaxchimp.min.js"></script>
+
+    <!-- Jquery Plugins, main Jquery -->
+    <script src="assets/js/plugins.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
